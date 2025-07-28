259a073c550a5640027211ebe6248c1bc33fbcc1
Sidharth A <sidharth69122@gmail.com>
2025-07-28 17:55:02 +0530

    test commit, menu is back

--- Test2/src/Test.java
+++ Test2/src/Test.java
@@ -8,7 +8,7 @@ public class Test {
         boolean running = true;
 
         while (running) {
-            System.out.println("Menu Driven Program:");
+            System.out.println("Menu:");
             System.out.println("1. Add Student");
             System.out.println("2. Get Student by ID");
             System.out.println("3. Get Practical Marks by Student ID and Semester");
